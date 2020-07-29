d2fdd29e77fa1ee0ceeab39d55d5caa88b5cd37b
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-07-29 14:53:35 +0300

    basic-calc complete

--- 1. Basic/3) basic-calc/index.js
+++ 1. Basic/3) basic-calc/index.js
@@ -1,5 +1,10 @@
 function basicCalc(operation, a, b) {
-  // Напиши свой код здесь
+  switch (operation){
+      case ("+"): return a + b;
+      case ("-"): return a - b;
+      case ("*"): return a * b;
+      case ("/"): return a / b;
+  }
 }
 
 window.basicCalc = basicCalc;
