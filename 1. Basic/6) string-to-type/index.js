de0e2c203717ce96235b82db6ddc4badca4cd985
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-07-29 17:57:15 +0300

    string-to-type complete

--- 1. Basic/6) string-to-type/index.js
+++ 1. Basic/6) string-to-type/index.js
@@ -1,6 +1,23 @@
 function stringToType(str) {
-  // Напиши свой код здесь
-};
+    switch (str) {
+        case "null":
+            return null;
+        case "undefined":
+            return undefined;
+        case "true":
+            return true;
+        case "false":
+            return false;
+        case "":
+            return "";
+    }
+
+    if (!isNaN(Number(str))) {
+        return Number(str);
+    }
+
+    return str;
+}
 
 window.stringToType = stringToType;
 
