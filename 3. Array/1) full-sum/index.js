c7b1076524a20a079cd963d69fb9fe109eb4df15
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-07-30 11:49:49 +0300

    full-sum complete

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,12 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        if (typeof args[i] !== "number") {
+            throw ("Wrong Argument Type");
+        }
+        sum += args[i];
+    }
+    return sum;
 }
 
 window.fullSum = fullSum;
