9512ea1291fe2265abb269d025e9e740f569a892
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-07-30 09:38:47 +0300

    sum-check complete

--- 2. Data Types/1) sum-check/index.js
+++ 2. Data Types/1) sum-check/index.js
@@ -1,5 +1,8 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+  if (typeof a === "number" && typeof b === "number"){
+    return a + b;
+  } throw new Error("Wrong arguments type!")
+
 }
 
 window.sumCheck = sumCheck;
