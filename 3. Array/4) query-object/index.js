a0dd4c49bfc52d4f159fc2c2a03984287ef5a4ec
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-07-31 18:40:13 +0300

    string-to-type complete

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,7 +1,40 @@
 function queryToObject(query) {
-  // Напиши свой код здесь
-};
+    let arr = query.slice(1).split("&");
+    let obj = {};
+    let subArr;
 
+    for (let i = 0; i < arr.length; i++) {
+        subArr = arr[i].split("=");
+        obj[subArr[0]] = stringToType(subArr[1]);
+
+
+    }
+
+    return obj;
+
+}
+function stringToType(str) {
+    switch (str) {
+        case "null":
+            return null;
+        case "undefined":
+            return undefined;
+        case "true":
+            return true;
+        case "false":
+            return false;
+        case "":
+            return "";
+        default: {
+            if (!isNaN(Number(str))) {
+                return Number(str);
+            }
+            return str;
+        }
+    }
+
+
+}
 window.queryToObject = queryToObject;
 
 export default queryToObject;
