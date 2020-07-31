860acb22b7802d9a333b35abfcc19ac2b77fc0ca
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-07-31 12:54:32 +0300

    is-equal complete

--- 2. Data Types/2) is-equal/index.js
+++ 2. Data Types/2) is-equal/index.js
@@ -1,5 +1,29 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
+    if (a === null || b === null) {
+        return false;
+    }
+    if (Object.keys(a).length !== Object.keys(b).length) {
+        return false;
+    }
+    for (let key in a) {
+
+        if ((typeof a[key] === "object" || typeof b[key] === "object") && a[key] !== null && b[key] !== null) {
+            if (!isEqual(a[key], b[key])) {
+                console.log("1");
+                return false;
+            }
+        } else {
+            if (a[key] !== b[key]) {
+                return false;
+            }
+            console.log("yea boi");
+            console.log(key + " " + b[key]);
+            if (typeof a[key] === "undefined" && typeof b[key] === "undefined" && !b.hasOwnProperty(key)) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 window.isEqual = isEqual;
