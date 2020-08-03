55eb2c01b44922f77be82c42dc3b37503ec015d9
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-08-03 17:33:36 +0300

    async-sum complete

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,5 +1,12 @@
 function asyncSum(a, b) {
-  // Напиши свой код здесь
+    return new Promise((resolve, reject) => {
+        const isNumber = typeof a === "number" && typeof b === "number";
+        if (isNumber) {
+                resolve(a + b);
+        } else {
+            reject(new Error("not a number"));
+        }
+    })
 }
 
 window.asyncSum = asyncSum;
