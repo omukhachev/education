67a561a9a2aab1ef574b651983ecac3981da5267
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-08-03 12:11:13 +0300

    apply() replaced by spread

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -3,9 +3,10 @@ function arraysSort(arr) {
 }
 
 function arrayCompare(a, b) {
-    return Math.max.apply(null, a) - Math.max.apply(null, b);
+    return Math.max(...a) - Math.max(...b);
 }
 
 window.arraysSort = arraysSort;
 
 export default arraysSort;
+
