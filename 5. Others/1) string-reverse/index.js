5fd031848bef97033d08febf981336fa8574578e
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-08-04 14:09:37 +0300

    string-reverse complete

--- 5. Others/1) string-reverse/index.js
+++ 5. Others/1) string-reverse/index.js
@@ -1,8 +1,5 @@
-/*
-  Задача:
-  Добавить функции метод reverse, который будет переворачивать строку на оборот
-  Примеры:
-  'test'.reverse() === 'tset';
-  'bob'.reverse() === 'bob';
-  'abc123'.reverse() === '321cba';
- */
+String.prototype.reverse = function() {
+    return [...this].reverse().join("");
+}
+
+
