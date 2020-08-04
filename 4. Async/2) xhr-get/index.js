ea578713d564be01161dbb776dcbe2903342c3a2
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-08-04 11:13:33 +0300

    xhr-get complete

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,17 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.responseType = "json";
+        xhr.send();
+        xhr.onload = () => {
+            if (xhr.status.toString()[0] === "2") {
+                resolve(xhr.response);
+            }
+            reject(xhr.response);
+        }
+    })
 }
 
 window.xhrGet = xhrGet;
