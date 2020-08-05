e753d24f30f52563863e611f643a7e13273533b2
Oleg Mukhachev <omukhachev.exceedteam@mail.ru>
2020-08-05 09:46:49 +0300

    base-expect complete

--- 5. Others/2) base-expect/index.js
+++ 5. Others/2) base-expect/index.js
@@ -7,9 +7,21 @@
   baseExpect(5).toBe.not(5) === false
   Примечание: Должно работать только с числами
  */
-
 function baseExpect(a) {
-  // Напиши свой код здесь
+    baseExpect.toBe = (b) => {
+        if (!isNaN(a) && !isNaN(b)) {
+            return a === b;
+        }
+        return new Error("NaN");
+    };
+    baseExpect.toBe.not = (b) => {
+        if (!isNaN(a) && !isNaN(b)) {
+            return a !== b;
+        }
+        return new Error("NaN");
+    };
+    return baseExpect;
+
 }
 
 window.baseExpect = baseExpect;
